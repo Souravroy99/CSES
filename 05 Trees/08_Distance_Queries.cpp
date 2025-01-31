a248ce07038df76ce5289aa6fdf6a06ae58d4cab
Souravroy99 <souravroy200418@gmail.com>
2025-01-31 22:47:08 +0530

    EULER TOUR TECHNIQUE learned

--- 05 Trees/08_Distance_Queries.cpp
+++ 05 Trees/08_Distance_Queries.cpp
@@ -1,71 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 200001 ;
-int depth[N] ;
- 
-int level_up(vector<vector<int>>&up, int node, int level)
-{
-    while(level != 0 && node != -1)
-    {   
-        int first_bit = log2(level) ;
-        node = up[node][first_bit] ;
-        level ^= (1<<first_bit) ;
-    }
-
-    return node ;
-}
-
-void binary_lifting(vector<vector<int>>&up, int n)
+void sparse_table(int n, vector<pair<int,int>>&arr, vector<vector<pair<int,int>>>&sparseTable)
 {
     int LOG = log2(n) ;
+    for(int i=0 ; i<n ; i++) {
+        sparseTable[i][0] = arr[i] ;
+    }
 
     for(int level=1 ; level<=LOG ; level++)
     {
-        for(int child=1 ; child<n ; child++)
+        for(int i=0 ; i + (1<<level) <= n ; i++)
         {
-            if(up[child][level-1] == -1) continue;
-            up[child][level] = up[up[child][level-1]][level-1] ;
+            sparseTable[i][level] = min(sparseTable[i][level-1], sparseTable[i + (1<<(level-1))][level-1]) ;
         }
     }
 }
 
-void dfs_for_depth(vector<int>adj[], int node, int parent=-1, int d=0)
+void euler_tour(vector<int>adj[], vector<int>&firstOcc, vector<int>&eulerTour, vector<int>&depth, int node, int parent, int d)
 {
-    depth[node] = d ;
-    for(auto neighbour: adj[node]) {
-        if(neighbour == parent) continue;
-        dfs_for_depth(adj, neighbour, node, d+1) ;
+    firstOcc[node] = eulerTour.size() ;
+    eulerTour.push_back(node) ;
+    depth.push_back(d) ;
+
+    for(auto neighbour: adj[node])
+    {
+        if(neighbour == parent) continue ;
+        euler_tour(adj, firstOcc, eulerTour, depth, neighbour, node, d+1) ;
+
+        eulerTour.push_back(node) ;
+        depth.push_back(d) ;
     }
 }
 
 int main()
 {
-    int n, queries;
-    cin >> n >> queries;
-    
-    int LOG = log2(n);
-    vector<vector<int>>up(n, vector<int>(LOG+1, -1)) ;
-
+    int n, queries ;
+    cin >> n >> queries ;
     vector<int>adj[n] ;
 
-    for (int edge = 1; edge < n; edge++)
-    {
+    for(int i=1 ; i<n ; i++) {
         int node1, node2 ;
         cin >> node1 >> node2 ;
-        --node1, --node2 ; 
-
-        up[node2][0] = node1 ;
-
-        adj[node1].push_back(node2) ;        
-        adj[node2].push_back(node1) ;        
+        node1--, node2-- ;
+        adj[node1].push_back(node2) ;
+        adj[node2].push_back(node1) ;
     }
 
-    int startNode = 0 ;
-    dfs_for_depth(adj, startNode) ;
-    binary_lifting(up , n) ;
+    vector<int>firstOcc(n, -1) ;
+    vector<int>eulerTour, depth ;
 
-    vector<int>distance_between_nodes ;
+    euler_tour(adj, firstOcc, eulerTour, depth, 0, -1, 0) ; // Normal DFS
+
+
+    int m = depth.size(), LOG = log2(m) ;
+    vector<vector<pair<int,int>>>sparseTable(m, vector<pair<int,int>>(LOG+1)) ;
+    vector<pair<int,int>>depthIdx ;
+
+    for(int i=0 ; i<2*n ; i++) depthIdx.push_back({depth[i], i}) ;
+
+    sparse_table(m, depthIdx, sparseTable) ;
+
 
     while(queries--)
     {
@@ -73,39 +68,17 @@ int main()
         cin >> node1 >> node2 ;
         node1--, node2-- ;
 
-        int temp_node1 = node1 ;
-        int temp_node2 = node2 ;
+        int idx1 = firstOcc[node1], idx2 = firstOcc[node2] ;
+        if(idx1 > idx2) swap(idx1, idx2) ;
 
-        int depth_diff = abs(depth[node1] - depth[node2]) ;
+        int len = idx2-idx1+1 ;
+        int j = log2(len) ;
+ 
+        pair<int,int> minDepth = min(sparseTable[idx1][j], sparseTable[idx2 - (1<<j) + 1][j]) ; 
 
-        if(depth[node1] > depth[node2]) {
-            node1 = level_up(up, node1, depth_diff) ;
-        }
-        if(depth[node1] < depth[node2]) {
-            node2 = level_up(up, node2, depth_diff) ;
-        }
+        int lcaIdx = minDepth.second ;
+        int lcaNode = eulerTour[lcaIdx] ;
 
-
-        int level = LOG ; 
-        while(level >= 0)
-        {
-            if(up[node1][level] != up[node2][level])
-            {
-                node1 = up[node1][level] ;
-                node2 = up[node2][level] ;
-            }
-
-            level-- ;
-        }
-
-        int LCA = up[node1][0] ;
-
-        if(node1 == node2) LCA = node1 ;
-
-        // int dist = depth[temp_node1] + depth[temp_node2] - 2*depth[LCA] ;
-        // distance_between_nodes.push_back(dist) ;
+        cout << depth[idx1] + depth[idx2] - 2*depth[firstOcc[lcaNode]] << endl ;
     }
-
-    for(auto d: distance_between_nodes) 
-    cout << d << "\n" ;
 }
